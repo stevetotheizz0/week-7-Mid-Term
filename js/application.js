4d386ef5f3d907fa01a1fe6db493a9a17b71f087
stevetotheizz0 <stephenjskilton@gmail.com>
2016-03-05 21:06:30 -0500

    Done and Done
    Everything is finished. I might go back and try to do one or two more
    things.
    
    I would like a little input on a couple things:
    
    1.  What are a couple of things I could do to clean this up and make it
    more organized so that it is easier to read?
    
    2.  Is it possible to change the icon based on the final popup text
    input? (This seems tricky since its part of a feature group, and the way
    popups work.)

--- js/application.js
+++ js/application.js
@@ -3,12 +3,25 @@ var nextPage = '#slide2';
 var previousPage = '#slide1';
 var currentPage = '#slide1';
 
+var showNext = function() {
+  $(currentPage).hide();
+  $(nextPage).show();
+  switchNext();
+};
+
+var showPrevious = function() {
+  $(currentPage).hide();
+  $(previousPage).show();
+  switchPrev();
+};
+
 var switchNext = function(){
   switch (currentPage) {
       case '#slide1':
         currentPage = '#slide2';
         nextPage = '#slide3';
         previousPage = '#slide1';
+        $('#button-previous').show();
       break;
       case '#slide2':
         nextPage = '#slide4';
@@ -19,11 +32,14 @@ var switchNext = function(){
         nextPage = '#slide5';
         previousPage = '#slide3';
         currentPage = '#slide4';
+        setUpSlide4();
         break;
       case '#slide4':
         nextPage = '#slide5';
         previousPage = '#slide4';
         currentPage = '#slide5';
+        $('#button-next').hide();
+        setUpSlide5();
         break;
       case '#slide5':
         nextPage = '#slide5';
@@ -44,6 +60,7 @@ var switchPrev = function() {
         nextPage = '#slide2';
         previousPage = '#slide1';
         currentPage = '#slide1';
+        $('#button-previous').hide();
         break;
       case '#slide3':
         nextPage = '#slide3';
@@ -54,39 +71,74 @@ var switchPrev = function() {
         nextPage = '#slide4';
         previousPage = '#slide2';
         currentPage = '#slide3';
+        setUpSlide5();
         break;
       case '#slide5':
         nextPage = '#slide5';
         previousPage = '#slide3';
         currentPage = '#slide4';
+        setUpSlide4();
+        $('#button-next').show();
         break;
   }
 };
 
-var clicking = function(){console.log("clicking");};
+var myFeatureGroup;
+var parsedData;
+var numericField2;
 
 var changeMap = function(){
   if (currentPage === '#slide2'){
     numericField1 = $('#num1').val();
-    console.log("numericField1", numericField1);
     resetMap();
+    myFeatureGroup = L.geoJson(parsedData, {
+      filter: function(feature, layer) {
+        return parseInt(feature.properties.Rating) >= numericField1;
+      }
+    }).addTo(map);
+  }
+  if (currentPage === '#slide3'){
+    numericField2 = $('#num2').val();
+    resetMap();
+    myFeatureGroup = L.geoJson(parsedData, {
+      filter: function(feature, layer) {
+        return feature.properties.Num_Reviews >= numericField2;
+    }
+    }).addTo(map);
   }
 };
 
-var showNext = function() {
-  $(currentPage).hide();
-  $(nextPage).show();
-  switchNext();
+var setUpSlide4 = function(){
+  resetMap();
+  myFeatureGroup = L.geoJson(parsedData, {
+    filter: function(feature, layer) {
+      return feature.properties.Num_Reviews >= numericField2;
+  },
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(feature.properties.Name + '<br>' +"Rating:" + feature.properties.Rating + '<br>' + "Number of Reviews:" + feature.properties.Num_Reviews );}
+    }).addTo(map).on('click', function(e) {
+      map.setView(e.latlng, 20, {animate: true});
+    });
 };
 
-var showPrevious = function() {
-  $(currentPage).hide();
-  $(previousPage).show();
-  switchPrev();
+var setUpSlide5 = function(){
+  map.closePopup();
+  map.removeLayer(myFeatureGroup);
+  myFeatureGroup = L.geoJson(parsedData, {
+    filter: function(feature, layer) {
+      return feature.properties.Num_Reviews >= numericField2;
+  },
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(fireplaceCheckbox);}
+    }).addTo(map).on('click', function(e) {
+      map.setView(e.latlng, 20, {animate: true});
+    });
 };
 
-var myFeatureGroup;
-var parsedData;
+var fireplaceCheckbox = '<input type="checkbox" name="fireplace" value="wood"> Wood Fireplace<br><input type="checkbox" name="fireplace" value="gas" checked> Gas Fireplace<br><input id="fireplaceType" type="submit" value="Submit">' ;
+
+var fireplaceInput = function(){alert('Great Job! Thanks for letting us know all the deets about this fireplace.');};
+
 
 var getAndParseData = function() {
   $(document).ready(function() {
@@ -100,3 +152,10 @@ var getAndParseData = function() {
 var resetMap = function() {
   map.removeLayer(myFeatureGroup);
 };
+
+
+/*
+var eachFeature = function(feature, layer){if (feature.properties.Name !== "XFINITY Live! Philadelphia") {
+         layer.bindPopup(feature.properties.Name);} };
+var markers = _.filter(parsedData, function(num) {return feature.properties.FEATURE == 'NATURE OBSERVATION';});
+*/
--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 
 getAndParseData();
 
-
-$( "#my-button").click(function(){changeMap();});
+$('#button-previous').hide();
+$( ".my-button").click(function(){changeMap();});
 $( "#button-next").click(function(){showNext();});
-$( "#button-previous" ).click(function() {showPrevious(); console.log("it worked");});
+$( "#button-previous" ).click(function() {showPrevious();});
+$('#map').on('click', 'input[type=submit]', function() {fireplaceInput();});
